53f9593c952b43cc68e52bfd6598474d538c787d
Med <geek.4tech007@gmail.com>
2024-12-01 11:30:19 +0000

    💄 added theme

---
+++ lib/core/themes/app_colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xffE50812);
+  static const secondary = Color(0xff272b30);
+  static const primaryBackground = Color(0xff1a1d1f);
+  static const secondaryBackground = Color(0xff272b30);
+  static const primaryText = Color(0xffa9aaac);
+  static const secondaryText = Colors.white;
+  static const primaryBtnText = Colors.white;
+  static const error = Colors.red;
+  static const black = Colors.black;
+  static const inactive = Color(0x26ffffff);
+  static const transparent = Colors.transparent;
+  static const ratingIcon = Color(0xffffbe21);
+  static const circleDot = Color(0x33ffffff);
+  static const iconContainer = Color(0xB2272830);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:filmflix/core/themes/app_themes.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,13 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'FilmFlix',
       debugShowCheckedModeBanner: false,
-      darkTheme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFFE50812),
-          brightness: Brightness.dark,
-        ),
-        useMaterial3: true,
-      ),
+      darkTheme: AppThemes.darkTheme,
       themeMode: ThemeMode.dark,
       home: const Scaffold(),
     );
